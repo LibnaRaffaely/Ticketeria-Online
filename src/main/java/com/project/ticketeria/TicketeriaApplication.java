61a6a6df69d965ec725e1a5297215c209f137c00
LibnaRaffaely <libnaraffaelyy@gmail.com>
2025-10-09 22:04:57 -0300

    inicialização do projeto

---
+++ src/main/java/com/project/ticketeria/TicketeriaApplication.java
@@ -0,0 +1,13 @@
+package com.project.ticketeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketeriaApplication.class, args);
+	}
+
+}
